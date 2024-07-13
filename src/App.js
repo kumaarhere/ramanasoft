cb025c71b25ecf7f254cee880744386fc3dc1aa6
kumaarkandugula <kumaarkandugula@gmail.com>
2024-07-13 11:34:25 +0530

    RamanaSoft IT Sevices

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Hire from './components/Hire';
+import StaffHire from './components/StaffHire';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+       <BrowserRouter>
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/hire' element={<Hire/>}/>
+        <Route path='/about-staff-hiring' element={<StaffHire/>}/>
+
+       </Routes>
+       
+       </BrowserRouter>
     </div>
   );
 }
